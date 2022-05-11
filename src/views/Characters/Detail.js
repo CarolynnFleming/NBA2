e258c12841cb89dfe013db2fa8ca0f7ff4d802da
CarolynnFleming <cfcatpink@gmail.com>
2022-05-11 07:20:56 -0700

    returning name ect....

--- src/views/Characters/Detail.js
+++ src/views/Characters/Detail.js
@@ -21,6 +21,19 @@ export default function Detail() {
         fetchRmCharacters();
     }, []);
   return (
-    <div>Detail</div>
-  )
+    <>
+    <h1> The Planets and Stars are something we truly do not understand!</h1>
+    <Link to="/">Back to what is Known</Link>
+    {loading ? (
+        <p>Loading The Strange....</p>
+    ) : (
+        <article>
+            <h2>{rM.name}</h2>
+            <img src={rM.image}/>
+            <h4>{rM.gender}</h4>
+            <h4>{rM.species}</h4>
+        </article>
+    )}
+    </>
+  );
 }
