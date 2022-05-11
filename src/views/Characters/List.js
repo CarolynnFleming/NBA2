f1503e4a4aeabca6115d2258364ef3f5c81b0bf5
CarolynnFleming <cfcatpink@gmail.com>
2022-05-11 07:28:38 -0700

    state set

--- src/views/Characters/List.js
+++ src/views/Characters/List.js
@@ -1,1 +1,13 @@
+import { useEffect, useState } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
+export default function List() {
+    const past = useHistory();
+    const present = useLocation();
+    const gender = new URLSearchParams(location.search).get('gender') ?? 'all';
+    const [gods, setGods] = useState([]);
+    const [computing, setComputing] = useState(true);
+  return (
+    <div>List</div>
+  )
+}
