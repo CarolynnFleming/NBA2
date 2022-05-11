d6509653c5f48d7a4e7d6b0ce8cdbded817d3437
CarolynnFleming <cfcatpink@gmail.com>
2022-05-11 07:14:57 -0700

    fetching data from api

--- src/views/Characters/Detail.js
+++ src/views/Characters/Detail.js
@@ -8,6 +8,18 @@ export default function Detail() {
     const [rM, setRM] = useStat({});
     const [loading, setLoading] = useState(true);
     const { id } = useParams();
+
+    useEffect(() => {
+        const fetchRmCharacters = async() => {
+            const res = await fetch(
+                `https://rickandmortyapi.com/api/character/${id}`
+            );
+            const rmData = await res.json();
+            setRM(rmData);
+            setLoading(false);
+        };
+        fetchRmCharacters();
+    }, []);
   return (
     <div>Detail</div>
   )
