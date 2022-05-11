6edb82bf99c5033098952d35b4cf159931c611ca
CarolynnFleming <cfcatpink@gmail.com>
2022-05-11 07:45:16 -0700

    fetching api data and using location to find gender

--- src/views/Characters/List.js
+++ src/views/Characters/List.js
@@ -4,14 +4,34 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 export default function List() {
     const past = useHistory();
     const present = useLocation();
-    const gender = new URLSearchParams(location.search).get('gender') ?? 'all';
+    const gender = new URLSearchParams(present.search).get('gender') ?? 'all';
     const [gods, setGods] = useState([]);
     const [computing, setComputing] = useState(true);
 
     const genderChange = (event) => {
         history.pushState(`/?gender=${event.target.value}`);
     };
-  return (
-    <div>List</div>
-  )
+
+    useEffect(() => {
+        const captureRm = async () => {
+            setComputing(true);
+            const genderParams = new URLSearchParams(present.search).get('gender');
+
+            const lifeEquation = 
+            genderParams === 'all' || !genderParams
+            ? 'https://rickandmortyapi.com/api/character'
+            : `https://rickandmortyapi.com/api/character?gender=${genderParams}`;
+
+            const myAnswer = await fetch(lifeEquation);
+            const { results } = await myAnswer.json();
+
+            setGods(results)
+            setComputing(false);
+
+        };
+        captureRm();
+    }, [present.search])
+    
+
+    
 }
